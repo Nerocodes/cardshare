d4b4f4329b3166a00914d71dbccae97def7973b9
nerocodes <neropaulej@gmail>
2019-10-26 19:14:42 +0100

    setup router and added navbar

---
+++ src/pages/HowItWorks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HowItWorks = () => {
+    return (
+        <section className="register-page">
+            How It Works
+        </section>
+    )
+}
+
+export default HowItWorks;
