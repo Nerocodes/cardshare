296bb9d132c2b30152c316001f0904b8720adac8
nerocodes <neropaulej@gmail>
2019-10-26 19:25:14 +0100

    removed comments

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const MainNav = () => {
+    return (
+        <Navbar>
+            <Navbar.Brand href="/">
+            <img
+                src="https://res.cloudinary.com/drlcfqzym/image/upload/v1572110475/cardshare.png"
+                width="204"
+                height="84"
+                className="d-inline-block align-top"
+                alt="card share logo"
+            />
+            </Navbar.Brand>
+            <Nav className="ml-auto">
+                <Nav.Link href="/how-it-works">How it works</Nav.Link>
+                <Nav.Link href="/categories">Categories</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default MainNav;
