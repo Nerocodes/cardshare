916fd99068e3077aab256ae9c8afcba80c66f407
nerocodes <neropaulej@gmail>
2019-10-26 19:29:52 +0100

    removed comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,6 @@ import Nav from 'react-bootstrap/Nav';
 
 const MainNav = () => {
     return (
-<<<<<<< HEAD
-=======
-        // <nav className="main-nav">
-        //     <div className="logo-container">
-        //         <img src="https://res.cloudinary.com/drlcfqzym/image/upload/v1572110475/cardshare.png" alt="logo"></img>
-        //     </div>
-        //     <ul className="links">
-        //         <Link to="/how-it-works"><li>How it works</li></Link>
-        //         <Link to="/how-it-works"><li>Categories</li></Link>
-        //         <Link to="/how-it-works"><li>Register</li></Link>
-        //         <Link to="/how-it-works"><li>Login</li></Link>
-        //     </ul>
-        // </nav>
->>>>>>> 38777a9df1a0eb0ef9f7ae33794829d527b11c21
         <Navbar>
             <Navbar.Brand href="/">
             <img
