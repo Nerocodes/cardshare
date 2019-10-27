c5bb538d096724d6c1c849fa583360161716eb4d
sakeyehp <sakeyehp@yahoo.co.uk>
2019-10-27 09:54:21 +0000

    add register and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import {
+  Form, Button,
+} from 'react-bootstrap';
+import './login.css'
 const Login = () => {
     return (
         <section className="login-page">
-            Login
+            <Form>
+            <h2>Sign In</h2>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+            </Form>
         </section>
     )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import {
+    Form, Button,
+  } from 'react-bootstrap';
+  import './register.css'
 
 const Register = () => {
     return (
         <section className="register-page">
-            Register
+            <Form>
+            <h2>Register</h2>
+                    <Form.Group >
+                        <Form.Control type="text" placeholder="Enter first name" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Control type="text" placeholder="Enter last name" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Control type="text" placeholder="Enter Phone Number" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Control type="password" placeholder="Enter Password" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="password" placeholder="Confirm Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+            </Form>
         </section>
     )
 }
